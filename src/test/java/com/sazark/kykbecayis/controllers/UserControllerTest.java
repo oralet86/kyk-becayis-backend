ad3cc000c69373e978864ca6773565a6be1d2a19
oralet86 <merocks86@gmail.com>
2025-05-16 14:16:42 +0300

    Removed MockBean

--- src/test/java/com/sazark/kykbecayis/controllers/UserControllerTest.java
+++ src/test/java/com/sazark/kykbecayis/controllers/UserControllerTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,8 +30,16 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private UserService userService;
+
+    @TestConfiguration
+    static class UserControllerTestContextConfiguration {
+        @Bean
+        public UserService userService() {
+            return Mockito.mock(UserService.class);
+        }
+    }
 
     @Test
     @DisplayName("POST /api/users - Success")
