93d91974a5b94b934e6355aaccd115254315e9dc
oralet86 <merocks86@gmail.com>
2025-05-21 00:59:14 +0300

    Updated FirebaseService to use Id token instead of UID

--- src/main/java/com/sazark/kykbecayis/services/FirebaseService.java
+++ src/main/java/com/sazark/kykbecayis/services/FirebaseService.java
@@ -2,11 +2,15 @@ package com.sazark.kykbecayis.services;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FirebaseService {
-    public void validateUID(String uid) throws FirebaseAuthException {
-        FirebaseAuth.getInstance().getUser(uid);
+     //* Verify the Firebase ID token and return the UID.
+     //* Throws FirebaseAuthException if token is invalid.
+    public String verifyIdTokenAndGetUID(String idToken) throws FirebaseAuthException {
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        return decodedToken.getUid();
     }
 }
