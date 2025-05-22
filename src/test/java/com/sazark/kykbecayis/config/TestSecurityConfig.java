95bae0daa2574c661e080dae0dedf3348ce531e1
oralet86 <merocks86@gmail.com>
2025-05-22 14:46:15 +0300

    Modified TestSecurityConfig

--- src/test/java/com/sazark/kykbecayis/config/TestSecurityConfig.java
+++ src/test/java/com/sazark/kykbecayis/config/TestSecurityConfig.java
@@ -1,19 +1,23 @@
 package com.sazark.kykbecayis.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@TestConfiguration
+@Profile("test")
 public class TestSecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .securityMatcher("/**") // Optional, applies to all routes
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable); // This is the correct non-deprecated form
 
         return http.build();
     }
