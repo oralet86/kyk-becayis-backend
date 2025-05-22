a0db6f5f556ff56d95e0da322f45eea429b09323
oralet86 <merocks86@gmail.com>
2025-05-22 23:57:14 +0300

    Added a cmd runner to make someone an admin

--- src/main/java/com/sazark/kykbecayis/user/UserRepository.java
+++ src/main/java/com/sazark/kykbecayis/user/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByFirebaseUID(String firebaseUID);
+
+    Optional<User> findByEmail(String email);
 }
