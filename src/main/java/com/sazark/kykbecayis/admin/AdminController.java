516b9995403e2b233c2409dab4527f6a36b03fc6
oralet86 <merocks86@gmail.com>
2025-05-23 14:54:04 +0300

    Added a dummy admin panel to be implemented later

---
+++ src/main/java/com/sazark/kykbecayis/admin/AdminController.java
@@ -0,0 +1,14 @@
+package com.sazark.kykbecayis.admin;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminPanel() {
+        return ResponseEntity.ok("Welcome to the admin panel");
+    }
+}
